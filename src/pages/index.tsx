44403d31a8c6ad95e5fb31ee53d51c4c0be2cce9
Dailton Bastos <dailtonbastos@gmail.com>
2021-11-29 22:45:44 -0300

    chore: add initial structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | Dailton Bastos</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
